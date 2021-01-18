5104bd9d5df91575cbd289b51d410a4e10392eaf
Oriane Joeger <oriane.joeger@gmail.com>
2021-01-18 18:34:59 +0100

    my_print_digits

---
+++ my_print_digits.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void    my_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int     my_print_digits(void)
+{
+    char c = '0';
+    while (c <= '9')
+    {
+        my_putchar(c);
+        c++;
+    }
+    return 0;
+}
+
+int     main()
+{
+    my_print_digits();
+    return 0;
+}
